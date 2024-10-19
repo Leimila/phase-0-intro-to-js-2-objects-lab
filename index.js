ef1f886aab07e3ca496b36ed59d64ded14dfe01e
Leimila <leimilamitei2016@gmail.com>
2024-10-19 19:25:32 +0300

    js objects

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee= {
+    name: 'Paul',
+    streetAddress: 134 Ollo St
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]:value
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]=value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) { 
+    cons newEployee={...employee};
+    delete newEmployee[key];
+    returm newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
