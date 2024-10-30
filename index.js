65e163c199f900f9fe36febf4fce2dd97f78b855
Leimila <leimilamitei2016@gmail.com>
2024-10-30 12:23:05 +0300

    js 2 objects lab

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// Write your solution in this file!
-const employee= {
-    name: 'Paul',
-    streetAddress: 134 Ollo St
+let employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
 };
+
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return {
-        ...employee,
-        [key]:value
-    };
+    return { ...employee, [key]: value };
 }
+
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key]=value;
+    employee[key] = value;
     return employee;
 }
-function deleteFromEmployeeByKey(employee, key) { 
-    cons newEployee={...employee};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
     delete newEmployee[key];
-    returm newEmployee
+    return newEmployee;
 }
+
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
     return employee;
